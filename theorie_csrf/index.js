019cc27a2aa28b9f2e446570a6b2c606a1549777
slimmii <andie.similon@ap.be>
2021-05-06 08:52:17 +0200

    Add stuff

--- theorie_csrf/index.js
+++ theorie_csrf/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const session = require('express-session')
+
 const fs = require('fs')
 const app = express()
 const port = 3000
@@ -19,13 +20,15 @@ let users = [
     }
 ]
 
+let messages = [
+    
+]
 
 
 app.use(express.static('public'))
-app.use(session({ secret: SESSION_SECRET }))
+app.use(session({ secret: SESSION_SECRET, cookie: {httpOnly: false} }))
 app.use(express.json());
 app.use(express.urlencoded());
-//app.use(csrf({ cookie: true }));
 app.set('view engine', 'ejs');
 
 app.use((req, res, next) => {
@@ -59,8 +62,20 @@ app.post('/', (req, res) => {
 app.get('/main', (req, res) => {
     if (res.locals.user) {
         let user = users.find(user => user.username === req.session.user.username);
+        let myMessages = messages.filter(message => message.to.username === res.locals.user.username) ?? [];
         console.log(user);
-        res.render('main', { money: user.money, users: users});
+        res.render('main', { user: user, money: user.money, users: users, messages: myMessages});
+    } else {
+        res.send('You are not logged in');
+    }
+});
+
+app.get('/messages', (req, res) => {
+    if (res.locals.user) {
+        let myMessages = messages.filter(message => message.to.username === res.locals.user.username) ?? [];
+        
+        console.log(myMessages);
+        res.render('messages', { messages: myMessages});
     } else {
         res.send('You are not logged in');
     }
@@ -71,26 +86,41 @@ app.get('/logout', (req, res) => {
     res.render('login');
   });
 
+app.post('/sendMessage', (req,res) => {
+    let to = req.body.to;
+    let message = req.body.message;
+
+    let fromUser = users.find(user => user.username === req.session.user.username);
+    let toUser = users.find(user => user.username === to);
+
+    messages = [...messages, {
+        from: fromUser,
+        to: toUser,
+        message: message
+    }]
+
+    let user = users.find(user => user.username === req.session.user.username);
+    let myMessages = messages.filter(message => message.to.username === res.locals.user.username) ?? [];
+    res.render('main', { user: user, money: user.money, users: users, messages: myMessages});
+
+})
+
 app.post('/sendMoney', (req, res) => {
     if (res.locals.user) {
         let to = req.body.to;
         let money = req.body.money;
 
-        console.log(to);
-        console.log(money);
-
         let fromUser = users.find(user => user.username === req.session.user.username);
         let toUser = users.find(user => user.username === to);
-
-console.log(toUser);
 
         fromUser.money = parseInt(fromUser.money) - parseInt(money);
         toUser.money = parseInt(toUser.money) + parseInt(money);
         
 
         let user = users.find(user => user.username === req.session.user.username);
+        let myMessages = messages.filter(message => message.to.username === res.locals.user.username) ?? [];
 
-        res.render('main', { money: user.money, users: users});
+        res.render('main', { user: user, money: user.money, users: users, messages: myMessages});
     } else {
         res.send('You are not logged in');
     }
