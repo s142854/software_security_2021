f0d14152982341ca8d2241993af2a3fd4f1a5b00
s142854 <126661616+s142854@users.noreply.github.com>
2023-03-20 23:29:31 +0100

    Update security.js

--- labo_hashing/security.js
+++ labo_hashing/security.js
@@ -4,10 +4,13 @@ const utils = require('./utils');
 const salt = CryptoJS.lib.WordArray.random().toString();
 const iterations = 1;
 
-const hash = (password) => {
-    let hashedPasword = password; // TODO: Hash this pasword.
+let hash = (password) => {
+    let hashedPasword = CryptoJS.PBKDF2('dit is een paswoord', salt, {
+        iterations: 10000,
+      }).toString();
     return hashedPasword;
 }
+
 
 exports.hash = utils.time(hash);
 exports.salt = salt;
