65859827f6adfab988455a5dd25c3aaf775492dd
s142854 <126661616+s142854@users.noreply.github.com>
2023-03-20 23:32:14 +0100

    Update login.js

--- labo_hashing/login.js
+++ labo_hashing/login.js
@@ -1,14 +1,18 @@
 const {database} = require('./database');
 const security = require('./security');
 const readline = require('readline-sync');
+const CryptoJS = require('crypto-js');
 
 let login = (email, password) => {
     return database.find((user) => user.email === email && user.password === password);
 }
 
 const email = readline.question('email: ');
-const password = readline.question('password: ');
-
+let password = readline.question('password: ');
+let salt = CryptoJS.lib.WordArray.random();
+password = CryptoJS.PBKDF2('dit is een paswoord', salt, {
+    iterations: 10000,
+  }).toString();
 if (login(email, password)) {
     console.log('You are succesfully logged in.');
 } else {
